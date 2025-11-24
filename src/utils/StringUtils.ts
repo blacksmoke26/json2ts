185a6fdbb57a3e36e139729112d8cf5c3d40215e
Junaid Atari <mj.atari@gmail.com>
2025-11-24 21:31:10 +0500

    feat: add case conversion utils for string formatting

--- src/utils/StringUtils.ts
+++ src/utils/StringUtils.ts
@@ -1,9 +1,21 @@
- /**
+/**
   * String utility methods
   * @author Junaid Atari <mj.atari@gmail.com>
   * @copyright 2025 Junaid Atari
   * @see https://github.com/blacksmoke26
   */
+import { camelCase, kebabCase, pascalCase, snakeCase } from 'change-case';
+
+/**
+ * Supported case naming conventions for transforming strings.
+ * - camel: camelCase (first letter lowercase, subsequent words capitalized)
+ * - lower_snake: snake_case (all lowercase with underscores)
+ * - original: unchanged case (preserves original casing)
+ * - pascal: PascalCase (first letter and subsequent words capitalized)
+ * - upper_snake: UPPER_SNAKE_CASE (all uppercase with underscores)
+ * - kebab: kebab-case (all lowercase with hyphens)
+ */
+export type CaseType = 'camel' | 'lower_snake' | 'original' | 'pascal' | 'upper_snake' | 'kebab';
 
 export default abstract class StringUtils {
   /**
@@ -17,4 +29,38 @@ export default abstract class StringUtils {
     return String(str || '') // convert it to string to prevent the unexpected error
       .charAt(0).toUpperCase() + str.slice(1);
   }
+
+   /**
+    * Formats a string according to the specified case type.
+    *
+    * @param name - The string to format
+    * @param [caseType] - The case type to apply ('camel', 'lower_snake', 'pascal', 'upper_snake', 'kebab', or undefined)
+    * @returns The formatted string in the specified case, or the original name if caseType is undefined
+    *
+    * @example
+    * ```typescript
+    * StringUtils.formatName('userName', 'camel') // returns 'userName'
+    * StringUtils.formatName('userName', 'lower_snake') // returns 'user_name'
+    * StringUtils.formatName('userName', 'pascal') // returns 'UserName'
+    * StringUtils.formatName('userName', 'upper_snake') // returns 'USER_NAME'
+    * StringUtils.formatName('userName', 'kebab') // returns 'user-name'
+    * StringUtils.formatName('userName', undefined) // returns 'userName'
+    * ```
+    */
+   public static formatName(name: string, caseType: CaseType | undefined = undefined): string {
+     switch (caseType) {
+       case 'camel':
+         return camelCase(name);
+       case 'lower_snake':
+         return snakeCase(name);
+       case 'pascal':
+         return pascalCase(name);
+       case 'upper_snake':
+         return snakeCase(name).toUpperCase();
+       case 'kebab':
+         return kebabCase(name);
+       default:
+         return name;
+     }
+   }
 }
